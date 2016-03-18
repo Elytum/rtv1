fc8e6f2229b90abec9fc5c838b7ced0dc51a4614
Arthur CHAZAL <achazal@e2r1p8.42.fr>
2016-03-18 19:54:35 +0100

    Gotta eat

--- srcs/forms/plane/hit.c
+++ srcs/forms/plane/hit.c
@@ -3,6 +3,8 @@
 
 int				hit_plane(const t_ray ray, const t_plane plane, float *t)
 {
+	const	float d = vec3_dot(normal, coord);
+
 	return (0);
 	(void)ray;
 	(void)plane;
@@ -32,6 +34,23 @@ int				hit_plane(const t_ray ray, const t_plane plane, float *t)
 	// }
 	// return (ret);
 }
+
+// bool linePlaneIntersection(Vector& contact, Vector ray, Vector rayOrigin, 
+//                            Vector normal, Vector coord) {
+//     // get d value
+//     float d = Dot(normal, coord);
+
+//     if (Dot(normal, ray) == 0) {
+//         return false; // No intersection, the line is parallel to the plane
+//     }
+
+//     // Compute the X value for the directed line ray intersecting the plane
+//     float x = (d - Dot(normal, rayOrigin)) / Dot(normal, ray);
+
+//     // output contact point
+//     *contact = rayOrigin + normalize(ray)*x; //Make sure your ray vector is normalized
+//     return true;
+// }
 
 void			find_closest_plane(t_scene scene, const t_ray ray, int *closest, float *t)
 {
